19480d22a591a06c159a1b19fd9adcd447f5ca82
Aviral Vardiya <aviralvardiya10@gmail.com>
2024-02-07 17:07:49 +0530

    signup,signin,update user,fetch user completed

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const server = express()
 const rootRouter = require("./routes/index")
 const cors = require("cors")
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const authMiddleware = require("./middlewares");
+
 
 server.use(cors())
 server.use(bodyParser.json())
 
 server.use("/api/v1",rootRouter)
+
+server.get("/testkaro",authMiddleware,(req,res)=>{
+    res.json({msg:"you have reached test route"})
+})
 
 
 
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { User } = require("../db");
+const { User, Account } = require("../db");
 const router = express.Router();
 const z = require("zod");
 const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middlewares");
+const mongoose = require("mongoose");
 
 router.post("/signup", async (req, res) => {
   // const user = new User(req.body)
@@ -39,14 +41,14 @@ router.post("/signup", async (req, res) => {
 
 router.post("/signin", async (req, res) => {
   const userCreds = req.body;
-  const fetchedUser =await User.findOne({ username: userCreds.username });
-  console.log(fetchedUser)
+  const fetchedUser = await User.findOne({ username: userCreds.username });
+  console.log(fetchedUser);
   const userObj = z.object({
-    username:z.string().email(),
-    password:z.string().min(8)
-  })
-  const {success,data} = userObj.safeParse(userCreds)
-  if(success){
+    username: z.string().email(),
+    password: z.string().min(8),
+  });
+  const { success, data } = userObj.safeParse(userCreds);
+  if (success) {
     if (fetchedUser) {
       if (fetchedUser.password == userCreds.password) {
         const token = jwt.sign(
@@ -63,19 +65,53 @@ router.post("/signin", async (req, res) => {
       res.status(404).json({
         message: "Error while logging in",
       });
-      
     }
-  }
-  else{
+  } else {
     res.status(404).json({
       message: "Error while logging in ekdum hi",
     });
   }
-  
 });
 
-router.get("/update", (req, res) => {
-  res.send("you can update here");
+router.post("/update", authMiddleware, async (req, res) => {
+  let userToBeUpdated = {};
+  if (req.body.password) {
+    userToBeUpdated["password"] = req.body.password;
+  }
+  if (req.body.firstName) {
+    userToBeUpdated["firstName"] = req.body.firstName;
+  }
+  if (req.body.lastName) {
+    userToBeUpdated["lastName"] = req.body.lastName;
+  }
+  if (userToBeUpdated.password && userToBeUpdated.password.length < 8) {
+    res.status(441).json({ msg: "password too small" });
+  }
+  const updatedUser = await User.findOneAndUpdate(
+    { username: req.username },
+    userToBeUpdated,
+    { returnDocument: "after" }
+  );
+  res.status(200).json({ msg: "user update", user: updatedUser });
+});
+
+router.get("/bulk", authMiddleware, async (req, res) => {
+  const filter = req.query.filter || "";
+  const objId = mongoose.Types.ObjectId;
+  const userId = (filter.length===24)? new objId(filter):new objId(0);
+  const query = await User.find({
+    $or: [{ firstName: filter }, { lastName: filter }, { _id: userId}],
+  }).exec();
+  if (query) {
+    res.json({ users: query.map(user=>({
+      userName:user.username,
+      firstName:user.firstName,
+      lastName:user.lastName,
+      id:user.id
+    })) });
+  } else {
+    res.json({ msg: "user not found" });
+  }
 });
 
 module.exports = router;
